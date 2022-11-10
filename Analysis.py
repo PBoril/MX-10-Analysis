cbcd0dedd9bd6a5318033cbda9814d4a8869a689
PBoril <107136833+PBoril@users.noreply.github.com>
2022-11-10 21:53:32 +0100

    Add files via upload

--- Analysis.py
+++ Analysis.py
@@ -1,84 +1,54 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun May 15 01:24:24 2022
+Created on Thu Sep 29 19:05:26 2022
 
 @author: pbori
 """
-import scipy
-from scipy import ndimage as ndi
-from skimage.feature import peak_local_max
-from skimage import data, img_as_float
-import mapfunctions
 import numpy as np
-C=256
-R=256
-def Saturation(InputFile):
-    global SensorSaturationVar
-    Zeros=0
-    for i in range (R):
-        for j in range(C):
-            if InputFile[i][j]==0:
-                Zeros=Zeros+1
-    return (100*(1-Zeros/(R*C)))
-def PixelCount(InputFile):
-    Pixels=0
-    for i in range(R):
-        for j in range(C):
-            if InputFile[i][j]!=0:
-                Pixels=Pixels+1
-    return(Pixels)
-def ObjectsCount(ObjectArray):
-    return np.amax(ObjectArray)
-def TotalEnergy(InputFile):
-    Energy=0
-    for i in range(R):
-        for j in range(C):
-            Energy=Energy+InputFile[i][j]
-    return (Energy)
-def Parametres(InputFile, ObjectArray):
-    DetectionList1=[]
-    MAXV=np.amax(ObjectArray)
-    for k in range (MAXV):
-        DetectionList1.append([k+1, 0, 0, 0,0])
-        
-    for i in range(R):
-        for j in range(C):
-            for k in range (MAXV):
-                if ((k+1)==ObjectArray[i][j]):
-                    DetectionList1[k][1]=DetectionList1[k][1]+1
-                    DetectionList1[k][2]=DetectionList1[k][2]+InputFile[i][j]
-    return DetectionList1
-def TypesAnalysis(InputFile, ObjectArray, DetectionList):
-    for i in range (np.amax(ObjectArray)):
-        if DetectionList[i][1]<5:
-            DetectionList[i][3]=3
-        if DetectionList[i][1]>=5:
-            if DetectionList[i][2]<=1000:
-                DetectionList[i][3]=2  
-        if DetectionList[i][2]>1000:
-            Alpha=1
-            if Alpha==1:
-                DetectionList[i][3]=1
-            if Alpha==0:
-                DetectionList[i][3]=5
+def ObjectRegister(InputArray, ObjectsArray):
+    DetectionList=[]
+    R=len(InputArray)
+    C=len(InputArray[0])
+    SupportDet=[0,0,0,0,0,0]   #ObjectID, PixCount, Energy, Diameter, ParticleType, Border
+    for k in range (np.amax(ObjectsArray)):
+        DetectionList.append(list(SupportDet))
+        DetectionList[k][0]=k+1
+    for k in range (len(DetectionList)):
+        for i in range(R):
+            for j in range(C):
+                if ObjectsArray[i][j]==k+1:
+                    DetectionList[k][1]=DetectionList[k][1]+1
+                    DetectionList[k][2]=DetectionList[k][2]+InputArray[i][j]
     return DetectionList
-def DetectOverlap(DataArray, ObjectArray, ObjectN):
-    SuppMatrix=[]
+
+def CreateObjectArray(InputArray, ObjectsArray, ObjectNumber):
+    R=len(InputArray)
+    C=len(InputArray[0])
+    SingleObjectArray=[ [0] * R for _ in range(C)]
     for i in range(R):
-        temp11=[]
-        for j in range (C):
-            temp11.append(0)
-        SuppMatrix.append(temp11)
-    for i in range (R):
-        for j in range (C):
-            if ObjectArray[i][j]==ObjectN:
-                SuppMatrix[i][j]=DataArray[i][j]
-    Peaks=peak_local_max(np.array(SuppMatrix), min_distance=3,threshold_rel=0.45)
-    if (len(Peaks)>1):
-        return (True)
-    if (len(Peaks)<2):
-        return (False)
+        for j in range(C):
+            if ObjectsArray[i][j]==ObjectNumber:
+                SingleObjectArray[i][j]=InputArray[i][j]
+    return SingleObjectArray
+    
 
 
-    
-    
+def BasicSeparation(InputArray, ObjectsArray, ObjectsList):
+    for i in range (len(ObjectsList)):
+        
+        if ObjectsList[i][2]>500:
+            if ObjectsList[i][1]<100:
+                ObjectsList[i][4]=1
+            if ObjectsList[i][1]>=100:
+                ObjectsList[i][4]=5
+        if ObjectsList[i][2]<=500:
+            if ObjectsList[i][1]<30:
+                ObjectsList[i][4]=2
+            if ObjectsList[i][1]>=30:
+                ObjectsList[i][4]=4
+        if ObjectsList[i][1]<4:
+            ObjectsList[i][4]=3
+            
+            
+            
+    return ObjectsList
